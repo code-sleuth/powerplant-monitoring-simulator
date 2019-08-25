66c8548669f32de9ec0ea90f7ed9f0d128f6a863
Ibrahim Mbaziira <code.ibra@gmail.com>
2019-08-25 05:27:07 +0300

    generate simulated data

--- src/distributed/sensors/sensor.go
+++ src/distributed/sensors/sensor.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 var (
 	name = flag.String("name", "sensor", "name of the sensor")
@@ -8,9 +14,36 @@ var (
 	max = flag.Float64("max", 5., "maximum value for generated readings")
 	min = flag.Float64("min", 1., "minimum value for generated readings")
 	stepSize = flag.Float64("step", 0.1, "maximum allowable change per measurement")
+
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	value = r.Float64() * (*max - *min) + *min
+	nom = (*max - *min) / 2 + *min
 )
 
 
 func main() {
 	flag.Parse()
+
+	duration, _ := time.ParseDuration(strconv.Itoa(1000/int(*freq)) + "ms")
+
+	signal := time.Tick(duration)
+
+	for range signal {
+		calcValue()
+		log.Printf("Reading sent. Value: %v\n", value)
+	}
+}
+
+func calcValue(){
+	var maxStep, minStep float64
+
+	if value < nom {
+		maxStep = *stepSize
+		minStep = -1 * *stepSize * (value - *min) / (nom - *min)
+	} else {
+		maxStep = *stepSize * (*max - value) / (*max - nom)
+		minStep = -1 * *stepSize
+	}
+
+	value += r.Float64() * (maxStep - minStep) + minStep
 }
